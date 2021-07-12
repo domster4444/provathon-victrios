37c225970382cbd4e6cddcd5e62d5d601f4ab20a
domster4444 <kshitiz.stha11@gmail.com>
2021-07-12 16:01:29 +0545

    (CONFIG):ESLINT CDN DEPENDENCIES

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import React from 'react';
-import './scss/main.css';
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
-export default function App() {
-  return (
-    <div>
-      <main id="Navigation">
-        <Accordion defaultActiveKey="0">
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="1">
-              Click me!
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey="1">
-              <Card.Body>Hello! I'm another body</Card.Body>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
-      </main>
-    </div>
-  );
-}
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
