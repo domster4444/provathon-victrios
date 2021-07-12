96a8aa96079b3286e639b8a1ce99d7b42bb04c94
domster4444 <kshitiz.stha11@gmail.com>
2021-07-13 00:13:52 +0545

    (FORM):: react-bootstrap form for all update page reused

--- src/components/5pages/Dashboard/UpdateTripleVacPatient.jsx
+++ src/components/5pages/Dashboard/UpdateTripleVacPatient.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
-
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
 const UpdateTripleVacPatient = () => {
-  return <div>update triple vac patient</div>;
+  return (
+    <main className="update-resource">
+      <div className="containerCenter">
+        <div className="contentBlock">
+          <Form>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Update No Of Triple Vaccinated Patient</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Update No Of Triple Vaccinated Patient"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox"></Form.Group>
+            <Button variant="primary" type="submit">
+              Update
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default UpdateTripleVacPatient;
