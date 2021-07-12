c265f420be9c8622f44ab52236550515b07c9f3c
domster4444 <kshitiz.stha11@gmail.com>
2021-07-12 16:06:56 +0545

    (INITIAL PHASE) front end start

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import React from 'react';
 
 const App = () => {
-  return <div></div>;
+  return <div>MY REACT ALPP</div>;
 };
 
 export default App;
