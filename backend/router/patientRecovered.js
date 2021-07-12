c36e682dfc88125591f2bd1177e2660940779f4c
domster4444 <kshitiz.stha11@gmail.com>
2021-07-12 23:55:58 +0545

    FIX: (ISSUE RECOVERED PATIENT)API:

--- backend/router/patientRecovered.js
+++ backend/router/patientRecovered.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const router = express.Router();
-//importing bcyrpt module for comparing password in db
+//importing bcrypt module for comparing password in db
 
 const patientRecoveredTemplate = require('../model/patientRecovered');
 const patientRecovered = require('../middleware/patientRecovered');
 
 //?MODEL WANTS
-//patientId
-//patient Name:
-//patient Doge:
-//patientAddress:
-//hotpitalPhNo:
-//hospitalDistrict:
+// patientId
+// patientName:
+// patientDoge:
+// patientAddress:
+// hotpitalPhNo:
+// hospitalDistrict:
 // hospitalEmail:
 // hospitalName:
 
 router.post('/patient/recovered', patientRecovered, (req, res) => {
   const patientId = req.body.data.patientId;
   const patientName = req.body.data.patientName;
-  const patientDoge = req.body.data.patienntDoge;
+  const patientDoge = req.body.data.patientDoge;
   const patientAddress = req.body.data.patientAddress;
   const hotpitalPhNo = req.body.data.hotpitalPhNo;
   const hospitalDistrict = req.body.data.hospitalDistrict;
-  const hospitalEmail = req.body.data.hospitalemail;
+  const hospitalEmail = req.body.data.hospitalEmail;
   const hospitalName = req.body.data.hospitalName;
-  //   _____________________________________to be saved patientDataToBeSavedBelow
+  // ---------------------------------to be saved patiendDataToBeSavedBelow
   const patientData = {
     patientId: patientId,
     patientName: patientName,
@@ -36,8 +36,8 @@ router.post('/patient/recovered', patientRecovered, (req, res) => {
     hospitalName: hospitalName,
   };
 
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++
-  //?CHECK IF EMAIL ALREADY EXIST IN DB OR NOT
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++
+  // ?CHECK IF EMAIL ALREADY EXIST IN DB OR NOT
   patientRecoveredTemplate
     .findOne({
       patientId: req.body.data.patientId,
@@ -46,16 +46,16 @@ router.post('/patient/recovered', patientRecovered, (req, res) => {
     })
     .then((data) => {
       if (data) {
-        console.log('--PatientRecoveredRoute .then Data');
+        console.log('--PatientRecoverdRoute .thenData');
         console.log(data);
         return res.status(200).json({
           message: 'Patient already added',
         });
       } else {
-        console.log('/patient recovered route triggered');
+        console.log('/patient/recovered route triggered');
 
-        //todo:------------save user to db with cluster name:
-        //________________________________SAVING REGISTERED USER DATA TO DB start
+        //todo:--------save user to db with cluster name :
+        // ______________________SAVING REGISTERED USER DATA TO DB start
         const registeredUserToBeSaved = new patientRecoveredTemplate(
           patientData
         );
@@ -64,13 +64,15 @@ router.post('/patient/recovered', patientRecovered, (req, res) => {
           .then((data) => {
             res.status(200).json({
               message: 'patient Data Saved Successfully',
+              data: data,
             });
           })
           .catch((error) => {
             res.json({
-              message: 'err occured while saving Patient  (DB Err)',
+              message: 'err occured while saving Patient (DB Err)',
             });
           });
+        // ______________________SAVING REGISTERED USER DATA TO DB end
       }
     });
 });
